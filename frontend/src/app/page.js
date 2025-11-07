cfb80c3ce39698f6f75083b5d4b9ee0d32a1179f
4CI_QUARTEY_MATTHEW_2024_25 <144942173+4CI-QUARTEY-MATTHEW-2024-25@users.noreply.github.com>
2025-11-07 14:23:03 +0100

    nuovo inizio

---
+++ frontend/src/app/page.js
@@ -0,0 +1,13 @@
+import MyHeader from './components/Assets/MyHeader';
+import MyFooter from './components/Assets/MyFooter';
+import TypingEffect from './components/Effects/TypingEffect';
+export default function Home() {
+  return (
+   <div className="mt-5">
+    <MyHeader />
+    
+    <MyFooter />
+   </div>
+  );
+}
+  
